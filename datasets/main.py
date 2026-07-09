d4b0ea91a50f792e4f19a87a6ac87564ecee0267
ANURAGVERMA9947 <anuragverma88772@gmail.com>
2026-07-09 12:27:24 +0530

    Add files via upload

--- datasets/main.py
+++ datasets/main.py
@@ -1,31 +1,16 @@
-from data_cleaning import clean_data
-from eda import *
-from sentiment_analysis import analyze_sentiment
+import os
 
-df = clean_data()
+files = [
+    "01_import_data.py",
+    "02_data_exploration.py",
+    "03_data_cleaning.py",
+    "04_category_analysis.py",
+    "05_rating_analysis.py",
+    "06_installs_analysis.py",
+    "07_price_analysis.py",
+    "08_sentiment_analysis.py"
+]
 
-print("\nDataset Shape")
-print(df.shape)
-
-print("\nMissing Values")
-print(df.isnull().sum())
-
-print("\nAverage Rating")
-print(df["Rating"].mean())
-
-print("\nAverage Price")
-print(df["Price"].mean())
-
-print("\nTop Categories")
-print(df["Category"].value_counts().head())
-
-top_categories(df)
-rating_distribution(df)
-installs_vs_rating(df)
-price_analysis(df)
-content_rating_analysis(df)
-correlation_heatmap(df)
-top_installed_apps(df)
-top_rated_apps(df)
-
-analyze_sentiment()
+for file in files:
+    print(f"\nRunning {file}")
+    os.system(f"python {file}")
